51f55ed9dd5a2aace9b38a2d2910342a859d0dc9
Dragoș Străinu <str.dr4605@gmail.com>
2022-03-12 13:37:28 +0200

    add docker-compose

--- service/main.py
+++ service/main.py
@@ -1,9 +1,10 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello from'
+    return 'Hello from ' + os.getenv('NAME')
 
 app.run(host='0.0.0.0', port=5001)
